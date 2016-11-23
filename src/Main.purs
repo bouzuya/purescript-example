84c70f853a3727b46a60941c34a7de7b8a6e7efe
bouzuya <m@bouzuya.net>
2016-11-24 08:56:56 +0900

    chapter 8 (wip)

--- src/Main.purs
+++ src/Main.purs
@@ -1,11 +1,20 @@
 module Main where
 
-import Prelude (Unit, show, bind, (<>))
+-- import Prelude (Unit, show, bind, (<>))
+-- import Control.Monad.Eff (Eff)
+-- import Control.Monad.Eff.Console (CONSOLE, log)
+-- import Control.Monad.Eff.Random (RANDOM, random)
+
+-- main :: forall e. Eff (console :: CONSOLE, random :: RANDOM | e) Unit
+-- main = do
+--   n <- random
+--   log ("Hello " <> show n)
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Eff.Random (RANDOM, random)
+import Prelude (Unit, void)
+import DOM (DOM())
 
-main :: forall e. Eff (console :: CONSOLE, random :: RANDOM | e) Unit
-main = do
-  n <- random
-  log ("Hello " <> show n)
+main :: Eff (console :: CONSOLE, dom :: DOM) Unit
+main = void do
+  log "Rendering address book component"
