5a022dce40f9e91e22e7683f3e29d173c6c47b8e
bouzuya <m@bouzuya.net>
2016-11-27 17:17:02 +0900

    chapter 8 (wip)

--- src/Main.purs
+++ src/Main.purs
@@ -1,20 +1,38 @@
 module Main where
 
--- import Prelude (Unit, show, bind, (<>))
--- import Control.Monad.Eff (Eff)
--- import Control.Monad.Eff.Console (CONSOLE, log)
--- import Control.Monad.Eff.Random (RANDOM, random)
-
--- main :: forall e. Eff (console :: CONSOLE, random :: RANDOM | e) Unit
--- main = do
---   n <- random
---   log ("Hello " <> show n)
+import Prelude
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Prelude (Unit, void)
+import Data.Maybe (fromJust)
+import Data.Nullable (toMaybe)
 import DOM (DOM())
+import DOM.HTML (window)
+import DOM.HTML.Types (HTMLDocument, htmlDocumentToNonElementParentNode)
+import DOM.HTML.Window (document)
+import DOM.Node.NonElementParentNode (getElementById)
+import DOM.Node.Types (Element, ElementId(..), NonElementParentNode)
+import Partial.Unsafe (unsafePartial)
+import React.DOM as D
+import ReactDOM (render)
 
-main :: Eff (console :: CONSOLE, dom :: DOM) Unit
+getMainElement :: forall eff. HTMLDocument -> Eff (dom :: DOM | eff) Element
+getMainElement htmlDocument = do
+  mainElement <- getElementById elementId nonElementParentNode
+  pure $ unsafePartial fromJust $ toMaybe mainElement
+  where
+  elementId :: ElementId
+  elementId = ElementId "main"
+  nonElementParentNode :: NonElementParentNode
+  nonElementParentNode = htmlDocumentToNonElementParentNode htmlDocument
+
+main :: Eff ( console :: CONSOLE
+            , dom :: DOM
+            ) Unit
 main = void do
   log "Rendering address book component"
+  let component = D.div [] [D.text "Hello, World!"]
+  w <- window
+  d <- document w
+  e <- getMainElement d
+  render component e
